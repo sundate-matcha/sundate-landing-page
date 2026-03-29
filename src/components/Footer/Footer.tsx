729c433ceec51387afa9724d793fb23cd5cbe391
nguyenkhoa-2306 <nguyenkhoa230604@gmail.com>
2026-03-29 16:16:14 +0700

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,104 +23,12 @@ function Footer({ isMenuVisible = false }: FooterProps) {
   return (
     <footer
       id={'contact'}
-      className={`md:px-10 px-4 py-4 text-center fixed bottom-0 left-0 w-full h-[7%] z-50 flex flex-row md:justify-between items-center transition-colors duration-500 ${
+      className={`md:px-10 px-4 py-4 text-center fixed bottom-0 left-0 w-full h-[7%] z-50 flex flex-row justify-between items-center transition-colors duration-500 ${
         isMenuVisible
           ? 'bg-black text-foreground'
           : 'bg-background text-foreground'
       }`}
     >
-      {/* <div
-        className={
-          'grid grid-cols-1 lg:grid-cols-2 mt-10 px-6 md:px-20 justify-around'
-        }
-      >
-        <div>
-          <a
-            href={'#'}
-            className={
-              'cursor-pointer justify-center flex items-center gap-2 flex-row md:flex-col '
-            }
-          >
-            <img src={Logo} alt="Sundate Logo" className={'md:h-20 h-14'} />
-
-            <img
-              src={LogoText}
-              alt="Sundate Logo Text"
-              className={'md:h-20 h-14'}
-            />
-          </a>
-        </div>
-        <div className="my-10 md:my-0 md:mb-10">
-          <div
-            className="text-center mb-5 cursor-pointer"
-            onClick={() => handleAddressClick(branch2)}
-          >
-            <FaLocationDot className="inline-block mr-2" />
-            <span>102B Tran Quoc Toan, Xuan Hoa Ward, HCM City</span>
-          </div>
-          <div
-            className="text-center mb-10 cursor-pointer"
-            onClick={() => handleAddressClick(branch1)}
-          >
-            <FaLocationDot className="inline-block mr-2" />
-            <span>
-              41 khu pho My Tu 3, Tan Phong Ward, District 7, HCM City
-            </span>
-          </div>
-
-          <div className="flex justify-center gap-10 mb-10">
-            <div
-              className="cursor-pointer"
-              onClick={() =>
-                window.open(
-                  'https://www.facebook.com/profile.php?id=61576618956568',
-                  '_blank',
-                )
-              }
-            >
-              <FaFacebook size={20} />
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() =>
-                window.open(
-                  'https://www.instagram.com/sundate.matchashelter',
-                  '_blank',
-                )
-              }
-            >
-              <FaInstagram size={20} />
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() =>
-                window.open(
-                  'https://www.threads.com/@sundate.matchashelter',
-                  '_blank',
-                )
-              }
-            >
-              <FaThreads size={20} />
-            </div>
-            <div
-              className="cursor-pointer"
-              onClick={() =>
-                window.open('https://www.tiktok.com/@sundatematcha', '_blank')
-              }
-            >
-              <FaTiktok size={20} />
-            </div>
-          </div>
-          <div
-            className="text-center mb-10 cursor-pointer"
-            onClick={() => window.open('tel:02812345678', '_blank')}
-          >
-            <FaPhone className="inline-block mr-2" />
-            <span>028 1234 5678</span>
-          </div>
-          <div className="text-center">MONDAY - SUNDAY: 08:20 - 22:20</div>
-        </div>
-      </div> */}
       {/* Left section */}
       <div className="flex items-center justify-center gap-4 md:gap-8">
         <div
@@ -139,8 +47,8 @@ function Footer({ isMenuVisible = false }: FooterProps) {
           <FaPhone className="inline-block" size={30} />
           <p className="hidden md:block text-2xl">hotline</p>
         </div>
-        <Separator orientation="vertical" className="h-10 bg-foreground" />
-        <div className="flex justify-center gap-4">
+        <Separator orientation="vertical" className="h-10 bg-foreground hidden md:block" />
+        <div className=" justify-center gap-4 hidden md:flex">
           <div
             className="cursor-pointer"
             onClick={() =>
@@ -186,6 +94,49 @@ function Footer({ isMenuVisible = false }: FooterProps) {
       </div>
 
       {/* Right section */}
+      <div className="flex justify-center gap-4 md:hidden">
+        <div
+          className="cursor-pointer"
+          onClick={() =>
+            window.open(
+              'https://www.facebook.com/profile.php?id=61576618956568',
+              '_blank',
+            )
+          }
+        >
+          <FaFacebook size={30} />
+        </div>
+        <div
+          className="cursor-pointer"
+          onClick={() =>
+            window.open(
+              'https://www.instagram.com/sundate.matchashelter',
+              '_blank',
+            )
+          }
+        >
+          <FaInstagram size={30} />
+        </div>
+        <div
+          className="cursor-pointer"
+          onClick={() =>
+            window.open(
+              'https://www.threads.com/@sundate.matchashelter',
+              '_blank',
+            )
+          }
+        >
+          <FaThreads size={30} />
+        </div>
+        <div
+          className="cursor-pointer"
+          onClick={() =>
+            window.open('https://www.tiktok.com/@sundatematcha', '_blank')
+          }
+        >
+          <FaTiktok size={30} />
+        </div>
+      </div>
       <div className={'hidden md:block text-center text-2xl'}>
         <p>
           <CopyrightIcon className={'size-4 inline'} /> 2026 Sundate. All rights
